314ec25b8e3058f2ffb0719d725bb9b8050dd352
Shefali Chaudhary <shefalichaudhary649@gmail.com>
2026-03-20 23:23:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "myec2" {
-  ami           = "ami-05d2d839d4f73aafb" # Change to a valid AMI for your region
+  ami           = "ami-0ec10929233384c7f" # Change to a valid AMI for your region
   instance_type = "c7i-flex.large"
 
   tags = {
