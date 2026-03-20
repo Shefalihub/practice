9adebb8b06554ae17a7e49483f5c23cb5688b959
Shefali Chaudhary <shefalichaudhary649@gmail.com>
2026-03-20 12:32:50 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-05d2d839d4f73aafb" # Change to a valid AMI for your region
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "TerraformInstance"
+  }
+}
